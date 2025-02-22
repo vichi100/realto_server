93a7a1838aa26496584605f2878777055704b47f
vichitra rajan <vichi100@gmail.com>
2025-02-22 16:54:10 +0530

    match

--- models/match/residentialBuyPropertyMatch.js
+++ models/match/residentialBuyPropertyMatch.js
@@ -4,11 +4,14 @@ const propertySchema = new mongoose.Schema({
   property_id: String,
   agent_id: String,
   matched_count: Number,
-  matched_customer_id_mine: [],
-  matched_customer_id_other: [],
-  create_date_time: {
-    type: Date
-  },
+  matched_customer_id_mine: [{
+    matched_percentage: Number,
+    customer_id: String
+  }],
+  matched_customer_id_other: [{
+    matched_percentage: Number,
+    customer_id: String
+  }],
   update_date_time: {
     type: Date
   }
